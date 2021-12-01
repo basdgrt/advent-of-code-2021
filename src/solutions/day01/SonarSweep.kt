fd403959f0c32e2c34835a688d300955fd0e9e9f
Bas de Groot <bdegroot@protonmail.com>
2021-12-01 20:03:22 +0100

    Remove redundant step size argument

--- src/solutions/day01/SonarSweep.kt
+++ src/solutions/day01/SonarSweep.kt
@@ -7,15 +7,15 @@ class SonarSweep : PuzzleSolution<Int>(fileName = "Day01.txt") {
 
     override fun firstStarSolution(puzzleInput: List<String>): Int {
         return puzzleInput.toInts()
-            .windowed(size = 2, step = 1)
+            .windowed(size = 2)
             .count { it[0] < it[1] }
     }
 
     override fun secondStarSolution(puzzleInput: List<String>): Int {
         return puzzleInput.toInts()
-            .windowed(size = 3, step = 1)
+            .windowed(size = 3)
             .map { it.sum() }
-            .windowed(size = 2, step = 1)
+            .windowed(size = 2)
             .count { it[0] < it[1] }
     }
 }
