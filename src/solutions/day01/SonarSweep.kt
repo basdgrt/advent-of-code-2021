f554654d36b22cf5b3050f90dd0ef90a93c5f001
Bas de Groot <bdegroot@protonmail.com>
2021-12-01 20:46:34 +0100

    Use transform argument instead of map

--- src/solutions/day01/SonarSweep.kt
+++ src/solutions/day01/SonarSweep.kt
@@ -8,14 +8,13 @@ class SonarSweep : PuzzleSolution<Int>(fileName = "Day01.txt") {
     override fun firstStarSolution(puzzleInput: List<String>): Int {
         return puzzleInput.toInts()
             .zipWithNext { current, next -> Pair(current, next) }
-            .count{ it.first < it.second}
+            .count { it.first < it.second }
     }
 
     override fun secondStarSolution(puzzleInput: List<String>): Int {
         return puzzleInput.toInts()
-            .windowed(size = 3)
-            .map { it.sum() }
+            .windowed(size = 3, transform = { it.sum() })
             .zipWithNext { current, next -> Pair(current, next) }
-            .count{ it.first < it.second}
+            .count { it.first < it.second }
     }
 }
---
+++ src/solutions/day02/Day2Solution.kt
@@ -0,0 +1,14 @@
+package solutions.day02
+
+import solutions.PuzzleSolution
+
+class Day2Solution : PuzzleSolution<Int>(fileName = "Day02_test.txt")  {
+
+    override fun firstStarSolution(puzzleInput: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun secondStarSolution(puzzleInput: List<String>): Int {
+        TODO("Not yet implemented")
+    }
+}
