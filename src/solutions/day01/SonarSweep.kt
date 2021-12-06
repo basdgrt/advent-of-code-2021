6e2dee007f8dc835fb609b7620058b6f835bcdb6
Bas de Groot <bdegroot@protonmail.com>
2021-12-06 18:01:49 +0100

    Get rid of useless extension functions

--- src/solutions/day01/SonarSweep.kt
+++ src/solutions/day01/SonarSweep.kt
@@ -1,18 +1,17 @@
 package solutions.day01
 
 import solutions.PuzzleSolution
-import util.toInts
 
 class SonarSweep : PuzzleSolution<Int>(fileName = "Day01.txt") {
 
     override fun firstStarSolution(puzzleInput: List<String>): Int {
-        return puzzleInput.toInts()
+        return puzzleInput.map { it.trim().toInt() }
             .zipWithNext { current, next -> Pair(current, next) }
             .count { it.first < it.second }
     }
 
     override fun secondStarSolution(puzzleInput: List<String>): Int {
-        return puzzleInput.toInts()
+        return puzzleInput.map { it.trim().toInt() }
             .windowed(size = 3, transform = { it.sum() })
             .zipWithNext { current, next -> Pair(current, next) }
             .count { it.first < it.second }
--- src/util/Utils.kt
+++ src/util/Utils.kt
@@ -4,7 +4,3 @@ import java.math.BigInteger
 import java.security.MessageDigest
 
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
-
-fun List<String>.toLongs() = map { it.toLong() }
-
-fun List<String>.toInts() = map { it.toInt() }
