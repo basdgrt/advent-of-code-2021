1885f189f1ecae27a9e3e8097e2e51c22c51ec23
Bas de Groot <bdegroot@protonmail.com>
2021-12-06 18:00:33 +0100

    Clean up day 6

--- src/solutions/day06/Lanternfish.kt
+++ src/solutions/day06/Lanternfish.kt
@@ -1,7 +1,6 @@
 package solutions.day06
 
 import solutions.PuzzleSolution
-import util.toInts
 
 class Lanternfish : PuzzleSolution<Long>(fileName = "Day06.txt") {
     override fun firstStarSolution(puzzleInput: List<String>): Long {
@@ -17,39 +16,46 @@ class Lanternfish : PuzzleSolution<Long>(fileName = "Day06.txt") {
     }
 }
 
+private fun List<Generation>.simulate(amountOfDays: Int): List<Generation> {
+    var generations = toList()
+
+    repeat(amountOfDays) { generations = generations.simulateSingleDay() }
+
+    return generations.toList()
+}
+
+private fun List<Generation>.simulateSingleDay(): List<Generation> {
+    val mutableGenerations = toMutableList()
+    mutableGenerations.forEach { it.decrementTimer() }
+
+    mutableGenerations.filter { it.isReadyToRespawn() }
+        .forEach { generationReadyToRespawn ->
+            mutableGenerations.removeIf { it.isReadyToRespawn() || it.is6thGeneration() }
+
+            val current6thGenFish = find { it.is6thGeneration() }?.numberOfFish ?: 0
+            val new6thGenFish = generationReadyToRespawn.numberOfFish
+            val updated6thGen = Generation(timer = 6, numberOfFish = current6thGenFish + new6thGenFish)
+
+            mutableGenerations.addAll(listOf(updated6thGen, generationReadyToRespawn.copy(timer = 8)))
+        }
+
+    return mutableGenerations.toList()
+}
+
 data class Generation(private var timer: Int, val numberOfFish: Long) {
     fun decrementTimer() {
-        timer -= 1
+        timer--
     }
 
     fun isReadyToRespawn() = timer < 0
 
-    fun hasTimerValue(expectedValue: Int) = timer == expectedValue
+    fun is6thGeneration() = timer == 6
 }
 
 private fun List<String>.mapToGenerations(): List<Generation> {
     return first()
         .split(",")
-        .toInts()
+        .map { it.trim().toInt() }
         .groupingBy { it }.eachCount()
         .map { Generation(timer = it.key, numberOfFish = it.value.toLong()) }
-}
-
-private fun List<Generation>.simulate(amountOfDays: Int): List<Generation> {
-    val generations = toMutableList()
-
-    repeat(amountOfDays) {
-        generations.forEach { it.decrementTimer() }
-        if (generations.any { it.isReadyToRespawn() }) {
-            val generationReadyToRespawn = generations.find { it.isReadyToRespawn() }
-                ?: throw IllegalStateException("Could not find generation to respawn")
-
-            generations.add(generationReadyToRespawn.copy(timer = 8))
-            val numberOfGeneration6Fish = generations.find { it.hasTimerValue(6) }?.numberOfFish ?: 0
-            generations.removeIf { it.hasTimerValue(6) }
-            generations.add(Generation(timer = 6, numberOfFish = generationReadyToRespawn.numberOfFish + numberOfGeneration6Fish))
-        }
-        generations.removeIf { it.isReadyToRespawn() }
-    }
-    return generations.toList()
 }
